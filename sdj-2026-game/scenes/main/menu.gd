04a4edf73878b9e69e7a01a7c763ceac049517b2
Axel Samuel Herrera Aguiar <asherrera11@espe.edu.ec>
2026-08-20 11:54:26 -0500

    menu completo

---
+++ sdj-2026-game/scenes/main/menu.gd
@@ -0,0 +1,39 @@
+extends Control
+
+@onready var panel_credits = $Panel_Credits
+@onready var panel_options = $Panel_Options
+@onready var audio_hover = $Audio_Hover
+
+func _ready():
+	# Ocultamos los paneles al iniciar
+	panel_credits.hide()
+	panel_options.hide()
+
+func _on_button_hovered():
+	audio_hover.play()
+
+# --- MAIN MENU BUTTONS ---
+
+func _on_button_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/level_base/level_base.tscn")
+
+func _on_button_options_pressed():
+	panel_options.show()
+
+func _on_button_credits_pressed():
+	panel_credits.show()
+
+# --- CLOSE BUTTONS FOR PANELS ---
+
+func _on_button_close_credits_pressed():
+	panel_credits.hide()
+
+func _on_button_close_options_pressed():
+	panel_options.hide()
+
+# --- VOLUME CONTROL ---
+
+func _on_slider_volume_value_changed(value):
+	# Convierte el valor del slider (0.0001 a 1) a decibelios y cambia el volumen general
+	var bus_index = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
