cf4cad466de7bba1199b7e30e9fb2b72b8d9a8e0
Axel Samuel Herrera Aguiar <asherrera11@espe.edu.ec>
2026-08-23 20:53:00 -0500

    Arreglo ded to be continued

--- sdj-2026-game/scenes/doors/exit/exit.gd
+++ sdj-2026-game/scenes/doors/exit/exit.gd
@@ -3,6 +3,8 @@ extends Area2D
 
 @export_file("*.tscn") var next_scene: String = ""
 @export var prompt_text: String = "Salir"
+@export var speaker: String = "Paciente N° 6174"
+@export_multiline var message: String = ""
 
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
@@ -45,11 +47,11 @@ func _on_all_fragments_collected() -> void:
 func get_prompt_text() -> String:
 	return prompt_text
 
-
 func interact(_player: Node) -> void:
-	if not _is_active:
-		return
-	print("¡El jugador llegó a la salida!")
-	if not next_scene.is_empty():
-		Transition.change_scene(next_scene)
+	Transition.play_open_door()
 
+	if not message.is_empty():
+		SignalHub.dialogue_requested.emit(speaker, message)
+		await SignalHub.ui_closed
+
+	SignalHub.ending_requested.emit()
